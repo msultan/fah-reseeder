0ea630c7c72bd0073c8af51f6a1f51e6cb58e702
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2015-04-20 11:06:11 -0700

    restart for clustering

--- cluster_project.py
+++ cluster_project.py
@@ -1,11 +1,17 @@
 from msmbuilder.cluster import MiniBatchKMeans
-from msmbuilder.utils import verbosedump
+from msmbuilder.utils import verbosedump,verboseload
 import numpy as np
+import os
 def cluster_project_wrapper(dir,feature_dict,n_states):
-     cluster_mdl = MiniBatchKMeans(n_clusters = n_states)
 
+     if os.path.exists(dir+"/assignments.pkl"):
+          return verboseload(dir+"/cluster_mdl.pkl"),verboseload(dir+"/assignments.pkl")
+     elif os.path.exists(dir+"/cluster_mdl.pkl"):
+          cluster_mdl = verboseload(dir+"/cluster_mdl.pkl")
+     else:
+          cluster_mdl = MiniBatchKMeans(n_clusters = n_states)
+          cluster_mdl.fit([feature_dict[i] for i in feature_dict.keys()])
 
-     cluster_mdl.fit([feature_dict[i] for i in feature_dict.keys()])
      assignments={}
      for i in feature_dict.keys():
           assignments[i] = cluster_mdl.transform([feature_dict[i]])
